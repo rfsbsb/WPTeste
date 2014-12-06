7cca26bb2e1c4dbb17807d51267fb172d1b7dfc5
Rafael Ferreira Silva <invites@rafaelsilva.net>
2014-12-06 01:41:44 -0200

    Initial commit

---
+++ App1/Model/Livro.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1.Model {
+  public class Livro {
+    public string Codigo { get; set; }
+    public string Titulo { get; set; }
+    public List<String> Capas { get; set; }
+    public List<String> Autores { get; set; }
+  }
+}
---
+++ App1/ViewModel/BaseVM.cs
@@ -0,0 +1,23 @@
+﻿using App1.Model;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace App1.ViewModel {
+    public class BaseVM {
+
+      public List<Livro> listaLivros;
+
+      public async Task GetListaLivros() {
+        var client = new HttpClient();
+        var uri = new Uri("http://services.toulendo.com.br/s/bn");
+        var response = await client.GetStringAsync(uri);
+        JArray resultArray = JArray.Parse(response);
+        this.listaLivros = resultArray.ToObject<List<Livro>>();
+      }
+
+    }
+}
